314e2bc340a368a3cb33f1a6b5ed947c01a4169c
SahanChamara <sahanchamara456@gmail.com>
2025-08-20 12:49:37 +0530

    Update API configuration and environment variables for improved clarity and structure

--- src/api/axiosConfig.tsx
+++ src/api/axiosConfig.tsx
@@ -4,6 +4,10 @@ const API_URL = import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },    
+    withCredentials: true
 })
 
 axiosInstance.interceptors.request.use((config) => {
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ import path from 'path'
 export default defineConfig({
     plugins: [ react() ],
     server: {
-        port: 3000,
+        port: 3000,  
+        allowedHosts: true,              
     },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
-    },
+    },    
 })
